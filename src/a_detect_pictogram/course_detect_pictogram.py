4acf9b34fa080ce9309467ed50599d03a83057ef
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2021-04-16 21:52:06 +0200

    remove progressbar, added some logs and comments

--- src/a_detect_pictogram/course_detect_pictogram.py
+++ src/a_detect_pictogram/course_detect_pictogram.py
@@ -2,37 +2,35 @@ import cv2
 import pyttsx3
 import logging
 import time
-from console_progressbar import ProgressBar
 
 from src.common.camera.camera import Camera
 
 logging.basicConfig(level=logging.INFO)
 
-# TODO Move config-elements to config.ini
-################################################################
 path_to_cascades = "../../resources/cascades/pictogram/"
 paths = ['hammer.xml', 'sandwich.xml', 'rule.xml', 'paint.xml', 'pencil.xml']  # PATH OF THE CASCADE
 objectNames = ['hammer', 'sandwich', 'rule', 'paint', 'pencil']  # OBJECT NAMES TO DISPLAY
 objectCount = 21  # how many objects to count for recognition
 
 
-#################################################################
-
-
 class PictogramDetector:
+    """
+    Class loads cascade files, analyzes the video stream and detects pictograms in front of the camera.
+    """
 
     def __init__(self):
         self.camera = Camera()
-        self.pb = ProgressBar(total=objectCount, prefix='Detection', length=42, fill='=', zfill=' ')
-
-        # LOAD THE CLASSIFIERS
         self.cascades = []
-        for c in paths:
+        for c in paths:  # LOAD THE CLASSIFIERS
             self.cascades.append(cv2.CascadeClassifier(path_to_cascades + c))
 
         logging.info("Ready for detection")
 
     def detect(self):
+        """
+        Used to detect and count pictograms.
+        :return: statistics of detected pictograms
+        """
         time.sleep(1)
         counter = 0
         stats = {'hammer': 0, 'sandwich': 0, 'rule': 0, 'paint': 0, 'pencil': 0}
@@ -50,11 +48,14 @@ class PictogramDetector:
                         counter += 1
                         stats[o] += 1
                         logging.debug(o)
-                        self.pb.print_progress_bar(counter)
         return stats
 
 
 def run():
+    """
+    Runs the PictogramDetector.
+    :return: the pictogram which had the most hits.
+    """
     detector = PictogramDetector()
     stats = detector.detect()
     logging.debug(stats)
@@ -64,6 +65,7 @@ def run():
     t2s.setProperty('voice', t2s.getProperty('voices'))
     comb = 'some' if result is 'paint' else 'a'
     t2s.say("I am looking for %s %s" % (comb, result))
+    logging.info("detected: %s", result)
     t2s.runAndWait()
     return result
 
