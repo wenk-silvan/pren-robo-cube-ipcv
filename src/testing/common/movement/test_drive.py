dbbbaab4cbde2daadd0c882f4bd0499436d12cdc
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2021-04-17 20:59:47 +0200

    added test_drive class - not functional yet

---
+++ src/testing/common/movement/test_drive.py
@@ -0,0 +1,69 @@
+import datetime
+import logging
+import time
+from unittest import TestCase
+
+from src.common.communication.serial_handler import SerialHandler
+from src.common.movement import drive
+from src.testing.common.communication import fake_serial
+
+logging.basicConfig(filename='test.log', filemode='w', level=logging.DEBUG)
+
+
+class TestDrive(TestCase):
+
+    def setUp(self):
+        """
+        Creates fake serial connection
+        """
+        self.serial_object = fake_serial.Serial()
+        self.serial_handler = SerialHandler(self.serial_object)
+        self.drive = drive.Drive(self.serial_handler)
+
+    def test_backward(self):
+        assert self.drive.backward(30) is True
+
+    def test_forward(self):
+        self.fail()
+
+    def test_forward_to_object(self):
+        self.fail()
+
+    def test_left(self):
+        self.fail()
+
+    def test_move(self):
+        self.fail()
+
+    def test_right(self):
+        self.fail()
+
+    def test_rotate_body_left(self):
+        self.fail()
+
+    def test_rotate_body_right(self):
+        self.fail()
+
+    def test_stop(self):
+        self.fail()
+
+    def test__rotate_front_wheels(self):
+        self.fail()
+
+    def test__rotate_back_wheels(self):
+        self.fail()
+
+    def test__rotate_all_wheels(self):
+        self.fail()
+
+    def test__drive(self):
+        self.drive._drive(0, 12)
+
+    def test__rotate_body(self):
+        self.fail()
+
+    def test__polling_motors(self):
+        self.fail()
+
+    def test__rotate_wheels(self):
+        self.fail()
