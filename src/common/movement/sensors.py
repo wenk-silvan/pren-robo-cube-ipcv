4f7ecfc6b05daf220cc0247baed0b0b418828da3
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2021-04-17 21:14:01 +0200

    change sensors class to return the distance correctly

--- src/common/movement/sensors.py
+++ src/common/movement/sensors.py
@@ -1,46 +1,37 @@
 import logging
-import time
 
 
 class Sensor:
     def __init__(self, serial_handler):
         self._serial_handler = serial_handler
-        pass
 
     def left(self):
         sensor_id = 0
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def front_left(self):
         sensor_id = 1
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def front_right(self):
         sensor_id = 2
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def right(self):
         sensor_id = 3
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def down_front(self):
         sensor_id = 4
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def down_center(self):
         sensor_id = 5
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def down_tail(self):
         sensor_id = 6
-        self._read_sensor(sensor_id)
-        pass
+        return self._read_sensor(sensor_id)
 
     def _read_sensor(self, sensor_id):
         """
@@ -50,9 +41,9 @@ class Sensor:
         """
         if sensor_id < 0 or sensor_id > 6:
             logging.warning("There is no Sensor with id[%s]", str(sensor_id))
-            pass
+            raise ValueError
 
         command = b'\x40' + sensor_id.to_bytes(1, byteorder='big', signed=False) + b'\x00'
-        distance = self._serial_handler.send_command(command)
+        answer = self._serial_handler.send_command(command)
 
-        return distance
+        return answer[2]
